2fb28d055ec5261183a8cc7c56eb04f87234defe
Barez-Abas-Osman <78170195+ABDULACOD3R@users.noreply.github.com>
2021-07-05 20:58:11 +0300

    Create DrawingCode.py

---
+++ DrawingCode.py
@@ -0,0 +1,18 @@
+import  os 
+os.system("clear ;pkg install jp2a ;clear")
+s= '''
+              c0000000Oc
+              Cdlkk0000O
+         .oxxxxxxxx0000O kkd'
+        .0KKK0000000000d 0KK0.
+        ,KKKKOllooooooll00KKK,
+        .0KKK oxxkkkOOO000KK0.
+         .oxx xxxkkooooddddc.
+              xxxkkxkld
+              Cdxkkkkc0o
+'''
+print(s)
+print("========image==to==text============")
+file=input("[ Path ] File ? >> ")
+print("==============SAMAD==================")
+os.system("jp2a "+file)
